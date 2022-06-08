b07bc4c4b8807859103178c6723b7ec2522078b8
Marial Clarie <marialclarie@gmail.com>
2022-06-08 19:16:57 +0300

    configure to do app with the main project

--- todoproject/settings.py
+++ todoproject/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todosapp'
+    'todos.app.TodosConfig'
 ]
 
 MIDDLEWARE = [
