0cec0e543ba31df419cbcbebafc090b597d6262d
Marial Clarie <marialclarie@gmail.com>
2022-06-09 14:20:04 +0300

    create add context to template htmls

--- todos/urls.py
+++ todos/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name="list"),
-	path('update_task/<str:pk>/', views.updateTask, name="update_task"),
-	path('delete/<str:pk>/', views.deleteTask, name="delete"),
+	path('update_task/<str:pk>/', views.update, name="update_task"),
+	path('delete/<str:pk>/', views.delete, name="delete"),
 ]
